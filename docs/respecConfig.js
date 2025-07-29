733db18e78d24d213476a5f231a49624f2260ef6
andre <andre@metasolutions.se>
2025-07-30 01:09:36 +0200

    [WIP] bikeshed and respec versions of the rules
    Note that in both cases modification of the original .md file is
    necessary and that neither fully supports the current markdown.

---
+++ docs/respecConfig.js
@@ -0,0 +1,36 @@
+var respecConfig = {
+    format: "markdown",
+    shortName: "INSPEC",
+    subtitle: "version 1.0",
+    specStatus: "LS",
+    lint: "false",
+    editors: [
+        {
+            name: "Matthias Palmér",
+            company: "MetaSolutions",
+            companyURL: "https://metasolutions.se/",
+            extras: [{
+                name: "Swedish Agency for Digital Government",
+                href: "https://www.digg.se/"
+            }]
+        },
+        {
+            name: "André Costa",
+            company: "MetaSolutions",
+            companyURL: "https://metasolutions.se/",
+            extras: [{
+                name: "Swedish Agency for Digital Government",
+                href: "https://www.digg.se/"
+            }]
+        },
+    ],
+    github: "diggsweden/interoperable-specifications",
+    otherLinks: [{
+        key: "Contributions from the reference group (in alphabetic order)",
+        data: [{
+            value: "Andrew Mercer (Riksantikvarieämbetet)"
+        },{
+            value: "Benny Lund (Bolagsverket)"
+        }]
+    }],
+}
