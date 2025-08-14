bf0c911cc1c07bbea4ea4e00cf7781b21aaac67f
andre <andre@metasolutions.se>
2025-08-14 09:06:59 +0200

    extend respec config

--- docs/respecConfig.js
+++ docs/respecConfig.js
@@ -1,36 +1,59 @@
 var respecConfig = {
-    format: "markdown",
-    shortName: "INSPEC",
-    subtitle: "version 1.0",
-    specStatus: "LS",
-    lint: "false",
-    editors: [
-        {
-            name: "Matthias Palmér",
-            company: "MetaSolutions",
-            companyURL: "https://metasolutions.se/",
-            extras: [{
-                name: "Swedish Agency for Digital Government",
-                href: "https://www.digg.se/"
-            }]
-        },
-        {
-            name: "André Costa",
-            company: "MetaSolutions",
-            companyURL: "https://metasolutions.se/",
-            extras: [{
-                name: "Swedish Agency for Digital Government",
-                href: "https://www.digg.se/"
-            }]
-        },
-    ],
-    github: "diggsweden/interoperable-specifications",
-    otherLinks: [{
-        key: "Contributions from the reference group (in alphabetic order)",
-        data: [{
-            value: "Andrew Mercer (Riksantikvarieämbetet)"
-        },{
-            value: "Benny Lund (Bolagsverket)"
-        }]
-    }],
+  format: "markdown",
+  shortName: "INSPEC",
+  subtitle: "version 1.0",
+  specStatus: "LS",
+  lint: "false",
+  editors: [
+    {
+      name: "Matthias Palmér",
+      company: "MetaSolutions",
+      companyURL: "https://metasolutions.se/"
+    },
+    {
+      name: "André Costa",
+      company: "MetaSolutions",
+      companyURL: "https://metasolutions.se/"
+    },
+  ],
+  github: "diggsweden/interoperable-specifications",
+  otherLinks: [
+    {
+      key: "On behalf of",
+      data: [{
+        value: "Swedish Agency for Digital Government",
+        href: "https://www.digg.se"
+      }]},
+    {
+      key: "Contributions from the reference group (in alphabetic order)",
+      data: [
+        {value: "Andrew Mercer (Riksarkivet)"},
+        {value: "Benny Lund (Bolagsverket)"},
+        {value: "Caspar Almalander (Sveriges Kommuner och Regioner)"},
+        {value: "Cilla Öhnfeldt (Naturvårdsverket)"},
+        {value: "Erik Mossing (Bolagsverket)"},
+        {value: "Fredrik Emanuelsson (Riksarkivet)"},
+        {value: "Fredrik Klingwall (Kungliga biblioteket)"},
+        {value: "Fredrik Persäter (Lantmäteriet)"},
+        {value: "Jan Rydkvist (Statistikmyndigheten SCB)"},
+        {value: "Johan Oelrich (Arbetsförmedlingen)"},
+        {value: "Kristofer Gäfvert (Lantmäteriet)"},
+        {value: "Lars Näslund (Trafikverket)"},
+        {value: "Marcus Smith (Riksarkivet)"},
+        {value: "Marjan Akhavan (E-hälsomyndigheten)"},
+        {value: "Martin Brandhagen (Vetenskapsrådet)"},
+        {value: "Matthias Palmér (Myndigheten för digital förvaltning)"},
+        {value: "Mattias Ekhem (Myndigheten för digital förvaltning)"},
+        {value: "Michalis Vassilas (Myndigheten för digital förvaltning)"},
+        {value: "Olof Olsson (Svensk Nationell Datatjänst)"},
+        {value: "Olov Johansson (Sveriges geologiska undersökning)"},
+        {value: "Patrik Wahlgren (Statistikmyndigheten SCB)"},
+        {value: "Per Wiklander (Vetenskapsrådet)"},
+        {value: "Peter Israelsson (Myndigheten för digital förvaltning)"},
+        {value: "Stefan Jakobsson (Svensk nationell datatjänst)"},
+        {value: "Tomas Lindberg (Sveriges geologiska undersökning)"},
+        {value: "Ulrika Domellöf Mattson (Myndigheten för digital förvaltning)"}
+      ]
+    }
+  ],
 }
