9c75555d98dc7299ee24515add6f8e67eb572b59
andre <andre@metasolutions.se>
2025-08-20 10:52:48 +0200

    hide Latest editor's draft

--- docs/config.js
+++ docs/config.js
@@ -3,6 +3,7 @@ var respecConfig = {
   shortName: "inspec",
   latestVersion: "http://w3id.org/inspec/specification",
   specStatus: "base",
+  edDraftURI: null,  // to prevent automatic generation
   historyURI: null,
   lint: {
     "no-headingless-sections": false,
